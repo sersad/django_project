f7c84f5ce3d15d56f269664908cb8d9935205664
Sergey Sadovnikov <sergey.sadovnikov@gmail.com>
2022-07-26 00:36:17 +0300

    fix stupid error

--- pg/pgadmin/startup.sh
+++ pg/pgadmin/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # mkdir -p /var/lib/postgresql/data/
-mount -t ext2 -o loop /pg_admin.img /var/lib/pgadmin
+mount -t ext2 -o loop pgadmin.img /var/lib/pgadmin
 #USER pgadmin
 /entrypoint.sh
