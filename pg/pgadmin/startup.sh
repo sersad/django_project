ac0d1c79d0f9eea95dc00eac02f7d81b96fbecc3
Sergey Sadovnikov <sergey.sadovnikov@gmail.com>
2022-07-26 01:10:00 +0300

    fix stupid error!

--- pg/pgadmin/startup.sh
+++ pg/pgadmin/startup.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-# mkdir -p /var/lib/postgresql/data/
-mount -t ext2 -o loop pgadmin.img /var/lib/pgadmin
-#USER pgadmin
+mount -t ext2 -o loop /pgadmin.img /var/lib/pgadmin
 /entrypoint.sh
--- pg/postgres/startup.sh
+++ pg/postgres/startup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # mkdir -p /var/lib/postgresql/data/
-mount -t ext2 -o loop postgres.img /var/lib/postgresql/data/
+mount -t ext2 -o loop /postgres.img /var/lib/postgresql/data/
 docker-entrypoint.sh postgres
